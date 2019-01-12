d0e5a3afb2f0cbbaad3b235aef84c531b7bc868d
Ha3 <git@ha3.eu>
2019-01-12 18:30:23 +0100

    (evolve) add tests for World

--- src/world.rs
+++ src/world.rs
@@ -28,3 +28,31 @@ impl World {
         return World { tick: 0, cursor_pos: Pos { x: 0, y: 0 } }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_should_update_the_tick() {
+        let mut subject = World::new();
+
+        // Exercise
+        subject.next_tick();
+
+        // Verify
+        assert_eq!(subject.current_tick(), 1)
+    }
+
+    #[test]
+    fn it_should_write_new_cursor_position() {
+        let mut subject = World::new();
+
+        // Exercise
+        subject.write_cursor_pos(&Pos { x: 100, y: 200 });
+
+        // Verify
+        assert_eq!(subject.get_cursor_pos().x, 100);
+        assert_eq!(subject.get_cursor_pos().y, 200);
+    }
+}
